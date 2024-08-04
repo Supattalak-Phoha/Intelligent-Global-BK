a15b243356ce6362a55492a208303297c4b0fc58
Supattalak-Phoha <57586534+Supattalak-Phoha@users.noreply.github.com>
2024-08-05 00:43:26 +0700

    Update data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -13,6 +13,11 @@ export class DataService {
     return this.http.get<any>(apiUrl);
   }
 
+  updateJson(data: any): Observable<any> {
+    const apiUrl = 'assets/data/data.json'; // เส้นทางไปยังไฟล์ JSON
+    return this.http.put( apiUrl, data);  // This won't work for local JSON files
+  }
+
   async login(username: string, password: string) {
     const apiUrl = 'assets/data/users.json'; // เส้นทางไปยังไฟล์ JSON
     const users: any[] = await this.http.get<any>(apiUrl)?.toPromise();
